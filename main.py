e42e236dc197e9cb2fd1edc98e95867d446e9bc8
BekaUn <bekayunusov@gmail.com>
2024-07-08 18:36:20 +0500

    python file was added

---
+++ main.py
@@ -0,0 +1,147 @@
+# print("Hello world")
+# print('Hello world')
+# print("I don't  speak english")
+# print('My name "Behzod')
+#
+# firs_name = "Behzod"
+# last_name = "Yunusov"
+# age: int = 20
+# print(firs_name +" " + last_name+" " +  str(age))
+#
+# pi = 3.14
+
+# print(type(firs_name))
+# print(type(last_name))
+# print(type(age))
+# print(type(pi
+# TODO:
+
+# name = ("Hello \n world")
+# n2 = """Hello
+# World
+# """
+#
+# first_name = "Behzod"
+# last_name = "Yunusov"
+# age = 20
+#
+# # print(first_name + " " + last_name+" " + str(age))
+#
+#
+# print(f'{first_name} {last_name} {age}')
+#
+#
+#
+#
+# string1 = """Hello world
+#     python
+#     name
+#
+#
+#     """
+#
+# print(string1)
+#
+# string2= ("Hello world \n"
+#           "\tpython\n\tname")
+# print(string2)
+
+
+# a = 23
+# b = 232
+# c = a * b
+# print(c)
+
+# name = input("Enter your name: ")
+# last_name = input("Enter your last name: ")
+#
+# print(name)
+# print(last_name)
+
+# number1 = int(input("Enter the number: "))
+# number2 = int(input("Enter the second number: "))
+#
+#
+# print(number1 + number2)
+#
+# fist_name = input("Name: ")
+# last_name = input("Last_name: ")
+# age = int(input("Age: "))
+# address = input("Address")
+# print(f"{fist_name} {last_name} {age} {address}")
+#
+
+
+# result = 2 + 3
+# result1 = 2 - 1
+# result2 = 10 / 5
+# result3 = 5 * 3
+#
+# result4 = 5 ** 2
+#
+# result5 = 9 % 6
+
+# print(result)
+# print(result1)
+# print(result2)
+# print(result3)
+# print(result4)
+# print(result5)
+
+text = "I love python"
+
+# print(text[0])
+# print(text[-1])
+#
+# # [start:stop:step]
+#
+# print(text[3:7:2])
+# print(text)
+# print(text[::-2])
+
+string1 = 'i Love pYtHon'
+# print(string1.lower())
+# print(string1.upper())
+# print(string1.capitalize())
+
+
+# string2 = string1.lower()
+# print(string1.isupper())
+# print(string2.islower())
+# print(string1.istitle())
+
+string5 = '99'
+
+# a = input("First number: ")
+# b = input("Second number: ")
+#
+# if a.isdigit() and b.isdigit():
+#     print(int(a) + int(b))
+#
+# else:
+#     print(a + b)
+
+var1 = True
+var2 = False
+
+print(type(var1))
+print(type(var2))
+
+# string6 = '                   i love python             '
+# string7 = string6.strip()
+# print(string7.startswith('i'))
+# print(string7.endswith('s'))
+
+text2 = 'Hello world'
+text3 = 'Python'
+text4 = '-'.join(text3)
+list1 = text2.split()
+print(text4)
+
+country = 'uzbekistan'
+
+user1 = input("Enter your country:")
+user2 = user1.lower()
+if country == user2:
+    print(True)
+
